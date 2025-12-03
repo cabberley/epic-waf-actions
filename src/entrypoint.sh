c4c98df14d7b816f6cb987d10e2bae43ef90e9a2
Chris Abberley <26394346+cabberley@users.noreply.github.com>
2025-12-03 11:59:17 +1000

    Create entrypoint.sh

---
+++ src/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+function parse_inputs {
+    
+    waf_file_or_dir="WAF"
+    if [ "${INPUT_WAF_FILE_OR_DIR}" != "" ] || [ "${INPUT_WAF_FILE_OR_DIR}" != "." ]; then
+        waf_file_or_dir="${INPUT_WAF_FILE_OR_DIR}"
+    fi
+}
+
+function main {
+
+    scriptDir=$(dirname ${0})
+    source ${scriptDir}/waf.sh
+    parse_inputs
+    
+    waf
+    
+}
+
+main "${*}"
