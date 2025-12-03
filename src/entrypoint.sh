25a42a3f6b9d1d47b20a37b2fbb9db645c43d15d
Chris Abberley <26394346+cabberley@users.noreply.github.com>
2025-12-03 13:32:18 +1000

    Update entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -6,13 +6,27 @@ function parse_inputs {
     if [ "${INPUT_WAF_FILE_OR_DIR}" != "" ] || [ "${INPUT_WAF_FILE_OR_DIR}" != "." ]; then
         waf_file_or_dir="--root ${INPUT_WAF_FILE_OR_DIR}"
     fi
-    yamllint_comment=0
-    if [[ "${INPUT_YAMLLINT_COMMENT}" == "0" || "${INPUT_YAMLLINT_COMMENT}" == "false" ]]; then
-        yamllint_comment="0"
+    waf_comment=0
+    if [[ "${INPUT_WAF_COMMENT}" == "0" || "${INPUT_WAF_COMMENT}" == "false" ]]; then
+        waf_comment="0"
     fi
 
-    if [[ "${INPUT_YAMLLINT_COMMENT}" == "1" || "${INPUT_YAMLLINT_COMMENT}" == "true" ]]; then
-        yamllint_comment="1"
+    if [[ "${INPUT_WAF_COMMENT}" == "1" || "${INPUT_WAF_COMMENT}" == "true" ]]; then
+        waf_comment="1"
+    fi
+    waf_run_validate_yaml_content=0
+    if [[ "${INPUT_WAF_RUN_VALIDATE_YAML_CONTENT}" == "0" || "${INPUT_WAF_RUN_VALIDATE_YAML_CONTENT}" == "false" ]]; then
+        waf_run_validate_yaml_content=0
+    fi
+    if [[ "${INPUT_WAF_RUN_VALIDATE_YAML_CONTENT}" == "1" || "${INPUT_WAF_RUN_VALIDATE_YAML_CONTENT}" == "true" ]]; then
+        waf_run_validate_yaml_content=1
+    fi
+    waf_run_create_excel_file=0
+    if [[ "${INPUT_WAF_RUN_CREATE_EXCEL_FILE}" == "0" || "${INPUT_WAF_RUN_CREATE_EXCEL_FILE}" == "false" ]]; then
+        waf_run_create_excel_file=0
+    fi
+    if [[ "${INPUT_WAF_RUN_CREATE_EXCEL_FILE}" == "1" || "${INPUT_WAF_RUN_CREATE_EXCEL_FILE}" == "true" ]]; then
+        waf_run_create_excel_file=1
     fi
 }
 
