ff92e7eebdb5d7affc5c7e5143dcfd29ff1a445b
Chris Abberley <26394346+cabberley@users.noreply.github.com>
2025-12-03 12:09:31 +1000

    Update entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -6,6 +6,14 @@ function parse_inputs {
     if [ "${INPUT_WAF_FILE_OR_DIR}" != "" ] || [ "${INPUT_WAF_FILE_OR_DIR}" != "." ]; then
         waf_file_or_dir="--root ${INPUT_WAF_FILE_OR_DIR}"
     fi
+    yamllint_comment=0
+    if [[ "${INPUT_YAMLLINT_COMMENT}" == "0" || "${INPUT_YAMLLINT_COMMENT}" == "false" ]]; then
+        yamllint_comment="0"
+    fi
+
+    if [[ "${INPUT_YAMLLINT_COMMENT}" == "1" || "${INPUT_YAMLLINT_COMMENT}" == "true" ]]; then
+        yamllint_comment="1"
+    fi
 }
 
 function main {
